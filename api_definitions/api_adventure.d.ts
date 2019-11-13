e53e8e6b33c895fafe84205b6293f5cd8afb8fb0
doctorgester <doctorgester@gmail.com>
2019-11-13 21:17:45 +0300

    Moved adventure handlers into api_adventure

--- api_definitions/api_adventure.d.ts
+++ api_definitions/api_adventure.d.ts
@@ -23,6 +23,73 @@ declare const enum Adventure_Party_Change_Type {
     set_health = 1
 }
 
+type Adventure_Handlers = {
+    type: Api_Request_Type.start_adventure
+    request: {
+        adventure_id: Adventure_Id
+    } & With_Token & With_Private_Key
+    response: Player_State_Data
+} | {
+    type: Api_Request_Type.enter_adventure_room
+    request: {
+        room_id: Adventure_Room_Id
+    } & With_Token & With_Private_Key
+    response: {}
+} | {
+    type: Api_Request_Type.exit_adventure
+    request: {} & With_Token & With_Private_Key
+    response: Player_State_Data
+} | {
+    type: Api_Request_Type.query_adventure_entity_states
+    request: {} & With_Token & With_Private_Key
+    response: {
+        states: Adventure_Entity_State[]
+    }
+} | {
+    type: Api_Request_Type.start_adventure_enemy_fight
+    request: {
+        enemy_entity_id: Adventure_Entity_Id
+    } & With_Token & With_Private_Key
+    response: Player_State_Data
+} | {
+    type: Api_Request_Type.submit_adventure_player_movement
+    request: {
+        current_location: {
+            x: number
+            y: number
+        }
+        movement_history: Movement_History_Entry[]
+    } & With_Token & With_Private_Key
+    response: {}
+} | {
+    type: Api_Request_Type.interact_with_adventure_entity
+    request: {
+        target_entity_id: Adventure_Entity_Id
+        starting_change_index: number
+    } & With_Token & With_Private_Key
+    response: {
+        party_updates: Adventure_Party_Change[]
+        updated_entity: Adventure_Entity_State
+    }
+} | {
+    type: Api_Request_Type.get_adventure_party_changes
+    request: {
+        starting_change_index: number
+    } & With_Token
+    response: {
+        changes: Adventure_Party_Change[]
+    }
+} | {
+    type: Api_Request_Type.adventure_party_cheat
+    request: {
+        starting_change_index: number
+        cheat: string
+    } & With_Token
+    response: {
+        party_updates: Adventure_Party_Change[]
+    }
+}
+
 type Adventure_Entity = Adventure_Entity_State & {
     definition: Adventure_Entity_Definition
 }
