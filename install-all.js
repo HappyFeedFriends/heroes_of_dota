567ee7b5fc74ee773ed67d4d9004627edc380dd3
doctorgester <doctorgester@gmail.com>
2019-11-25 14:25:26 +0100

    Updated submodule init

--- install-all.js
+++ install-all.js
@@ -2,6 +2,7 @@ const exec = require("child_process").execSync;
 
 const base = { stdio: "inherit" };
 
+exec("git submodule update --init --recursive", base);
 exec("git submodule update", base);
 exec("git submodule foreach git checkout master", base);
 exec("git submodule foreach git pull origin master", base);
